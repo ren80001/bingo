19f14c7cb449256acdc283db36b6e1b79a3aed31
taneichi <ren80001@gmail.com>
2021-01-05 18:48:44 +0900

    追加

--- bingo.py
+++ bingo.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import random
+import numpy as np
 
 '''ビンゴ表のリスト'''
 b = random.sample(range(1, 16), k=5)
@@ -21,17 +22,16 @@ random.shuffle(num)
 
 '''各カウントアップ'''
 count_up = 0
-bingo_count = 0
-reach_count = 0
 
 for b_num in num:
     count_up = count_up + 1
     if b_num in df1.values:
         df1 = df1.replace(b_num, 'X')
+
         print(str(count_up) + '回目！')
         print('ボールの番号は' + str(b_num) + 'です。当たりなので穴を開けます！')
-        print(df1)
 
+        '''縦判定'''
         for c in df1.columns:
             count = sum(df1[c] == 'X') + sum(df1[c] == 'FREE')
             if count == 5:
@@ -39,12 +39,24 @@ for b_num in num:
             elif count == 4:
                 print(f'{c}:リーチ！')
 
+        '''横判定'''
         for r in df1.index:
             count = sum(df1.loc[r] == 'X') + sum(df1.loc[r] == 'FREE')
             if count == 5:
                 print(f'{r}:ビンゴ！')
             elif count == 4:
                 print(f'{r}:リーチ！')
+
+        '''斜め判定'''
+        diagonal_r = list(np.diag(df1))
+        count_r = diagonal_r.count('X')
+
+        diagonal_l = list(np.diag(np.fliplr(df1)))
+        count2 = len(set(diagonal_l))
+
+
+        print(count2)
+
 
     else:
         print(str(count_up) + '回目！')
